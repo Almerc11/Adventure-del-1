8c88938afa4aa014342158aa9e53ee693e6dda0e
emila <zaaz@itu.dk>
2023-09-29 13:01:54 +0200

    Refactored

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,14 +3,49 @@ import java.util.ArrayList;
 public class Player {
     private Room currentRoom;
     private ArrayList<Item> inventory;
-    public Player(Map map){
+
+    public Player(Map map) {
         this.currentRoom = map.getStartRoom();
         inventory = new ArrayList<>();
     }
 
-    public void playerChoices(String userChoice){
+    public void playerChoices(String userChoice) {
         UserInterface UI = new UserInterface();
 
+        playUserDirections(UI, userChoice);
+
+        playUserInventoryManagement(UI, userChoice);
+
+        givePlayerHelp(UI, userChoice);
+
+       /* else if(userChoice.contains("drop")){
+            int count = 0;
+            for(Item item : inventory) {
+                count++;
+                int playerChoice = UI.whatItemToRemove(inventory);
+                if (inventory.get(count) == inventory.get(playerChoice)) {
+                    Item itemAddedBackInRoom = inventory.get(playerChoice);
+                    UI.removeItem(inventory.get(count));
+                    inventory.remove(count);
+                    currentRoom.getItemList().add(itemAddedBackInRoom);
+                } else if (inventory.get(count) != inventory.get(playerChoice)) {
+                    UI.removeItemError();
+                } else if (inventory.isEmpty()) {
+                    UI.noItemsLeftError();
+                }
+            }
+        } */
+    }
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void addItemToInventory(Item item) {
+        inventory.add(item);
+    }
+
+    public void playUserDirections(UserInterface UI, String userChoice) {
         if (userChoice.contains("go north")) {
             if (currentRoom.getNorth() != null) {
                 UI.giveNorthDirectionMessage();
@@ -39,11 +74,15 @@ public class Player {
             } else {
                 UI.giveErrorDirectionsMessage();
             }
-        } else if(userChoice.contains("look")){
-            UI.lookForDoors(currentRoom);
-        } else if(userChoice.contains("take")){
+        }
+    }
 
-            if(!currentRoom.getItemList().isEmpty()){
+    public void playUserInventoryManagement(UserInterface UI, String userChoice) {
+        if (userChoice.contains("look")) {
+            UI.lookForDoors(currentRoom);
+        } else if (userChoice.contains("take")) {
+
+            if (!currentRoom.getItemList().isEmpty()) {
                 Item item = currentRoom.getItemList().get(0);
                 addItemToInventory(item);
                 UI.takeItem(currentRoom.getItemList(), item);
@@ -51,39 +90,17 @@ public class Player {
             } else {
                 UI.noItemsLeftError();
             }
-        } else if(userChoice.contains("show")){
-            if(!inventory.isEmpty()){
+        } else if (userChoice.contains("show")) {
+            if (!inventory.isEmpty()) {
                 UI.showInventoryItems(inventory);
             } else {
                 UI.removeItemError(inventory);
             }
-        } else if(userChoice.contains("help")){
+        }
+    }
+    public void givePlayerHelp(UserInterface UI, String userChoice){
+        if (userChoice.contains("help")) {
             UI.help();
         }
-       /* else if(userChoice.contains("drop")){
-            int count = 0;
-            for(Item item : inventory) {
-                count++;
-                int playerChoice = UI.whatItemToRemove(inventory);
-                if (inventory.get(count) == inventory.get(playerChoice)) {
-                    Item itemAddedBackInRoom = inventory.get(playerChoice);
-                    UI.removeItem(inventory.get(count));
-                    inventory.remove(count);
-                    currentRoom.getItemList().add(itemAddedBackInRoom);
-                } else if (inventory.get(count) != inventory.get(playerChoice)) {
-                    UI.removeItemError();
-                } else if (inventory.isEmpty()) {
-                    UI.noItemsLeftError();
-                }
-            }
-        } */
-    }
-
-    public Room getCurrentRoom(){
-        return currentRoom;
-    }
-
-    public void addItemToInventory(Item item){
-        inventory.add(item);
     }
 }
